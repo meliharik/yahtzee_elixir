94c31d4cdc71b4067e15d00bc95db7f446a30ca3
Ege Kutay <egekyurusen@gmail.com>
2025-10-02 22:15:06 +0300

    Four of a kind - passing

--- lib/yahtzee.ex
+++ lib/yahtzee.ex
@@ -1,12 +1,21 @@
 defmodule Yahtzee do
   def score_lower(dice) do
     %{
-      "Three of a kind": three_of_a_kind(dice)
+      "Three of a kind": three_of_a_kind(dice),
+      "Four of a kind": four_of_a_kind(dice)
     }
   end
 
   defp three_of_a_kind(dice) do
-    if Enum.any?(Enum.frequencies(dice), fn {_k,v} -> v >= 3 end) do
+    if Enum.any?(Enum.frequencies(dice), fn {_k, v} -> v >= 3 end) do
+      Enum.sum(dice)
+    else
+      0
+    end
+  end
+
+  defp four_of_a_kind(dice) do
+    if Enum.any?(Enum.frequencies(dice), fn {_k, v} -> v >= 4 end) do
       Enum.sum(dice)
     else
       0
