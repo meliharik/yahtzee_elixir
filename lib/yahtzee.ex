8fcaaddeb2fb6323dacf0848ec5a7a80b4cd04f7
Melih Arık <meliharik@hotmail.com>
2025-10-02 22:16:23 +0300

    Full house - passing

--- lib/yahtzee.ex
+++ lib/yahtzee.ex
@@ -2,7 +2,8 @@ defmodule Yahtzee do
   def score_lower(dice) do
     %{
       "Three of a kind": three_of_a_kind(dice),
-      "Four of a kind": four_of_a_kind(dice)
+      "Four of a kind": four_of_a_kind(dice),
+      "Full house": full_house(dice)
     }
   end
 
@@ -20,5 +21,10 @@ defmodule Yahtzee do
     else
       0
     end
+  end
+
+  defp full_house(dice) do
+    counts = Enum.frequencies(dice) |> Map.values() |> Enum.sort()
+    if counts == [2, 3], do: 25, else: 0
   end
 end
