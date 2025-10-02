1509e16cc627575328281228489c49c7cb5a069d
Ege Kutay <egekyurusen@gmail.com>
2025-10-02 22:09:23 +0300

    Four of a kind -failing again

--- test/yahtzee_test.exs
+++ test/yahtzee_test.exs
@@ -19,31 +19,3 @@ defmodule YahtzeeTest do
     assert %{"Four of a kind": 0} = Yahtzee.score_lower([1, 2, 3, 3, 4])
   end
 end
-mix test   # patlar
-git add .
-git commit -m "Four of a kind - failing"
-yahtzee.ex
-defmodule Yahtzee do
-  def score_lower(dice) do
-    %{
-      "Three of a kind": three_of_a_kind(dice),
-      "Four of a kind": four_of_a_kind(dice)
-    }
-  end
-
-  defp three_of_a_kind(dice) do
-    if Enum.any?(Enum.frequencies(dice), fn {_k, v} -> v >= 3 end) do
-      Enum.sum(dice)
-    else
-      0
-    end
-  end
-
-  defp four_of_a_kind(dice) do
-    if Enum.any?(Enum.frequencies(dice), fn {_k, v} -> v >= 4 end) do
-      Enum.sum(dice)
-    else
-      0
-    end
-  end
-end
