06c72639f640042eb0ce20e208203f1086b8906c
Ege Kutay <egekyurusen@gmail.com>
2025-10-02 22:19:24 +0300

    Large straight - succcess

--- lib/yahtzee.ex
+++ lib/yahtzee.ex
@@ -3,28 +3,26 @@ defmodule Yahtzee do
     %{
       "Three of a kind": three_of_a_kind(dice),
       "Four of a kind": four_of_a_kind(dice),
-      "Full house": full_house(dice)
+      "Full house": full_house(dice),
+      "Large straight": large_straight(dice)
     }
   end
 
   defp three_of_a_kind(dice) do
-    if Enum.any?(Enum.frequencies(dice), fn {_k, v} -> v >= 3 end) do
-      Enum.sum(dice)
-    else
-      0
-    end
+    if Enum.any?(Enum.frequencies(dice), fn {_k, v} -> v >= 3 end), do: Enum.sum(dice), else: 0
   end
 
   defp four_of_a_kind(dice) do
-    if Enum.any?(Enum.frequencies(dice), fn {_k, v} -> v >= 4 end) do
-      Enum.sum(dice)
-    else
-      0
-    end
+    if Enum.any?(Enum.frequencies(dice), fn {_k, v} -> v >= 4 end), do: Enum.sum(dice), else: 0
   end
 
   defp full_house(dice) do
     counts = Enum.frequencies(dice) |> Map.values() |> Enum.sort()
     if counts == [2, 3], do: 25, else: 0
+  end
+
+  defp large_straight(dice) do
+    sorted = Enum.sort(Enum.uniq(dice))
+    if sorted == [1,2,3,4,5] or sorted == [2,3,4,5,6], do: 40, else: 0
   end
 end
