9dd44e6cb01e9bbee706ecd8babec6700b41a6be
Melih Arık <meliharik@hotmail.com>
2025-10-02 21:48:58 +0300

    Three of a kind - passing

--- lib/yahtzee.ex
+++ lib/yahtzee.ex
@@ -1,18 +1,15 @@
 defmodule Yahtzee do
-  @moduledoc """
-  Documentation for `Yahtzee`.
-  """
+  def score_lower(dice) do
+    %{
+      "Three of a kind": three_of_a_kind(dice)
+    }
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Yahtzee.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp three_of_a_kind(dice) do
+    if Enum.any?(Enum.frequencies(dice), fn {_k,v} -> v >= 3 end) do
+      Enum.sum(dice)
+    else
+      0
+    end
   end
 end
